7c016cec77aebb3a34fb3de533a252d1c50b576e
victorrf <victorrf2002@gmail.com>
2024-07-25 23:05:25 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+});
