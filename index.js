f1ad5bd054408ca5ac0a71f61eb73a0ad555b57c
victorrf <victorrf2002@gmail.com>
2024-07-30 18:22:06 -0400

    Finish styling random.ejs and index.ejs

--- index.js
+++ index.js
@@ -22,47 +22,52 @@ const API_URL = "https://collectionapi.metmuseum.org/public/collection/v1";
 
 app.post("/random", async (req, res) => {
 
-    // Try-catch block for axios
+    let success = false;
 
-    try {
+    while (!success) {
+        // Try-catch block for axios
+        try {
 
-        // Getting the total number of artworks in the MET museum
+            // Getting the total number of artworks in the MET museum
 
-        const getTotal = await axios.get(API_URL + "/objects");
-        const total = getTotal.data.total;
+            const getTotal = await axios.get(API_URL + "/objects");
+            const total = getTotal.data.total;
 
-        // Generate random artwork ID
+            // Generate random artwork ID
 
-        const randomID = Math.floor(Math.random() * total);
+            const randomID = Math.floor(Math.random() * total);
 
-        // Fetching the artwork given the randomID and creating a class for it with all the necessary attributes
+            // Fetching the artwork given the randomID and creating a class for it with all the necessary attributes
 
-        // const result = await axios.get(API_URL + "/objects/" + randomID);
-        const result = await axios.get("https://collectionapi.metmuseum.org/public/collection/v1/objects/45734");
-        const randomArtwork = result.data;
-        const artwork = {
-            "artID": randomArtwork.objectID,
-            "artTitle": randomArtwork.title,
-            "artImage": randomArtwork.primaryImage,
-            "artPeriod": randomArtwork.period,
-            "artArtistName": randomArtwork.artistDisplayName,
-            "artArtistBio": randomArtwork.artistDisplayBio,
-            "artDate": randomArtwork.objectDate,
-            "artCity": randomArtwork.city,
-            "artCountry": randomArtwork.country
-        };
+            const result = await axios.get(API_URL + "/objects/" + randomID);
+            // const result = await axios.get("https://collectionapi.metmuseum.org/public/collection/v1/objects/45734");
+            const randomArtwork = result.data;
+            const artwork = {
+                "artID": randomArtwork.objectID,
+                "artTitle": randomArtwork.title,
+                "artImage": randomArtwork.primaryImage,
+                "artPeriod": randomArtwork.period,
+                "artArtistName": randomArtwork.artistDisplayName,
+                "artArtistBio": randomArtwork.artistDisplayBio,
+                "artDate": randomArtwork.objectDate,
+                "artCity": randomArtwork.city,
+                "artCountry": randomArtwork.country
+            };
 
-        console.log(artwork);
-        res.render("random.ejs", { randomArtwork: artwork });
+            console.log(artwork);
+            res.render("random.ejs", { randomArtwork: artwork });
+            return;
 
-        // Error handling
+            // Error handling
 
-    } catch (error) {
-        console.error("Failed to make request:", error.message);
-        res.render("random.ejs", {
-            error: error.message
-        });
+        } catch (error) {
+            console.error("Failed to make request:", error.message);
+            res.render("random.ejs", {
+                error: error.message
+            });
+        }
     }
+
 })
 
 // Starting the server
